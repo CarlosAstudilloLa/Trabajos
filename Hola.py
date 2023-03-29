267c38c9d1be719009ae220daecaeaa4f82125e3
Carlos Astudillo <carlosjosueastulan@gmail.com>
2023-03-29 12:39:58 -0500

    version 10

--- Hola.py
+++ Hola.py
@@ -1,1 +1,9 @@
-print ("hola mundo")
+print = "hola mundo"
+print(dato)
+
+nombre = input("Ingrese nombre: ")
+apellido = input("Ingrese apellido: ")
+cedula = input("Ingrese cedula: ")
+print("Nombre",nombre,"nApellido",apellido,"nCedula",cedula)
+print(nombre*3)
+print(len(nombre*3))

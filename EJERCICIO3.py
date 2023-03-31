4d165425bb02734a5b1b6edd2d743b9906895c6f
Carlos Astudillo <carlosjosueastulan@gmail.com>
2023-03-31 11:08:41 -0500

    version 12

---
+++ EJERCICIO3.py
@@ -0,0 +1,9 @@
+print("FUNDAMENTOS DE PROGRAMACION DE PYTHON")
+print("Carlos Astudillo")
+print("30/03/2023")
+codigo="amor"
+contraseña=input("Ingrese la contraseña")
+if codigo==contraseña:
+    print("contraseña coincide")
+else:
+    print("constraseña no coincide")
---
+++ EJERCICIO4.py
@@ -0,0 +1,8 @@
+print("FUNDAMENTOS DE PROGRAMACION DE PYTHON")
+print("Carlos Astudillo")
+print("30/03/2023")
+entero=int(input("Ingrese su numero entero"))
+if entero % 2==0:
+    print("Es par")
+else:
+    print("Es impar")
